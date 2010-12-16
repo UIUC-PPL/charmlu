809e3ec928138011a57ab69de175e5c936a5879e
Ramprasad Venkataraman <ramv@illinois.edu>
2010-12-15 21:18:48 -0600

    Remove the exchange matrix completely from pivotSequencesMsg

--- lu.C
+++ lu.C
@@ -212,7 +212,6 @@ class pivotSequencesMsg: public CMessage_pivotSequencesMsg, public CkMcastBaseMs
         int numSequences;
         int *seqIndex;
         int *pivotSequence;
-        int *xchangeMatrix;
 
         pivotSequencesMsg(const int _firstRowProcessed, const int _numRowsProcessed)
         {
@@ -1306,12 +1305,11 @@ private:
       #endif
 
       // Create and initialize a msg to carry the pivot sequences
-      pivotSequencesMsg *msg = new(numRowsSinceLastPivotSend+1, numRowsSinceLastPivotSend*2, numBlks*numBlks, sizeof(int)*8)
+      pivotSequencesMsg *msg = new(numRowsSinceLastPivotSend+1, numRowsSinceLastPivotSend*2, sizeof(int)*8)
                                   pivotSequencesMsg(pivotBatchTag, numRowsSinceLastPivotSend);
       msg->numSequences = 0;
       memset(msg->seqIndex,      0, sizeof(int) * numRowsSinceLastPivotSend );
       memset(msg->pivotSequence, 0, sizeof(int) * numRowsSinceLastPivotSend*2 );
-      memset(msg->xchangeMatrix, 0, sizeof(int) * numBlks * numBlks );
 
       /// Parse the pivot operations and construct optimized pivot sequences
       int seqNo =-1, i = 0;
@@ -1333,17 +1331,11 @@ private:
               #ifdef VERBOSE_PIVOT_RECORDING
                   pivotLog<<" <-- "<<itr->second;
               #endif
-              // If the data flows across chares, the sender should know, so it can size its msgs
-              int fromIdx = msg->pivotSequence[i]   / BLKSIZE;
-              int toIdx   = msg->pivotSequence[i-1] / BLKSIZE;
-              msg->xchangeMatrix[fromIdx*numBlks + toIdx]++;
-
               std::map<int,int>::iterator prev = itr;
               itr = pivotRecords.find(itr->second);
               pivotRecords.erase(prev);
               i++;
           }
-          msg->xchangeMatrix[(chainStart/BLKSIZE)*numBlks + (itr->first/BLKSIZE)]++;
           pivotRecords.erase(itr);
           itr = pivotRecords.begin();
       }
