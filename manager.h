6d9a503533c77e8dc4b6ed29757151cb76eb1102
Jonathan Lifflander <jliffl2@illinois.edu>
2011-02-15 12:10:04 -0600

    Modify manages so it allows for pluggable priorities.

--- manager.h
+++ manager.h
@@ -2,12 +2,21 @@
 // Prefix notation:
 // b: an index into an LUBlk chare array
 
+enum PrioType {
+  SEND_PIVOT_DATA, PROCESS_TRAILING_UPDATE, RECVL, DIAG_SEND_PIVOT,
+  BELOW_SEND_USEG, DIAG_SEND_USEG, MULT_RECV_U, MULT_RECV_L,
+  PIVOT_RIGHT_SEC, PIVOT_LEFT_SEC, PIVOT_CRITICAL, PIVOT_NOT_CRITICAL
+};
+
 class LUMgr : public CBase_LUMgr
 {
 protected:
   int BLKSIZE;
   int matSize;
-  LUMgr(int BLKSIZE_, int matSize_) : BLKSIZE(BLKSIZE_), matSize(matSize_) {}
+  int numBlks;
+  LUMgr(int BLKSIZE_, int matSize_) : BLKSIZE(BLKSIZE_), matSize(matSize_) {
+    numBlks = matSize / BLKSIZE;
+  }
 
 public:
   /// Return a message with space for a block's data and the requested
@@ -18,6 +27,9 @@ public:
   {
     return new (BLKSIZE*BLKSIZE, priobits) blkMsg;
   }
+  virtual CkEntryOptions& setPrio(PrioType type, CkEntryOptions& opts,
+                                  int y = 0 , int x = 0) = 0;
+  virtual void setPrio(CkMessage *msg, PrioType type, int refnum = -1, int y = 0, int x = 0) = 0;
 };
 
 struct PrioLU : public LUMgr
@@ -31,5 +43,45 @@ struct PrioLU : public LUMgr
     if (0 != priobits)
       *((int*)CkPriorityPtr(msg)) = bactive;
     return msg;
+  }
+
+  CkEntryOptions& setPrio(PrioType type, CkEntryOptions& opts, int y = 0, int x = 0) {
+    int prio = 0;
+    switch (type) {
+    case SEND_PIVOT_DATA: case DIAG_SEND_PIVOT:
+      prio = -1;
+      break;
+    case PROCESS_TRAILING_UPDATE: case RECVL:
+      prio = y * BLKSIZE;
+      break;
+    }
+    opts.setPriority(prio);
+    return opts;
+  }
+
+  void setPrio(CkMessage *msg, PrioType type, int refnum = -1, int y = 0, int x = 0) {
+    int prio = 0;
+    switch (type) {
+    case DIAG_SEND_USEG: case BELOW_SEND_USEG: case MULT_RECV_U: case MULT_RECV_L:
+      prio = -1;
+      break;
+    case PIVOT_RIGHT_SEC:
+      prio = (y + 1) * BLKSIZE;
+      break;
+    case PIVOT_LEFT_SEC:
+      prio = BLKSIZE * numBlks + 1;
+      break;
+    case PIVOT_CRITICAL:
+      prio = y * BLKSIZE;
+      break;
+    case PIVOT_NOT_CRITICAL:
+      prio = numBlks * BLKSIZE;
+      break;
+    }
+    *(int*)CkPriorityPtr(msg) = prio;
+    CkSetQueueing(msg, CK_QUEUEING_IFIFO);
+    // Better way than this?
+    if (refnum != -1)
+      CkSetRefNum(msg, refnum);
   }
 };
