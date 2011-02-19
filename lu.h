652a4f104a5644f84d9dafb31ce08de7bdcf47c4
Jonathan Lifflander <jliffl2@illinois.edu>
2011-02-19 11:55:56 -0600

    State machine advance functions implemented.

--- lu.h
+++ lu.h
@@ -1,8 +1,5 @@
 #include "lu.decl.h"
-#include <vector>
 #include <list>
-#include <utility>
-#include <map>
 #include <algorithm>
 
 struct BlockReadyMsg : public CkMcastBaseMsg, CMessage_BlockReadyMsg {
@@ -13,6 +10,37 @@ struct BlockReadyMsg : public CkMcastBaseMsg, CMessage_BlockReadyMsg {
   CkIndex2D src;
 };
 
+struct BlockState {
+  // Index of block
+  int ix, iy;
+  // Count of trailing updates completed
+  int updatesCompleted;
+  // Have pivots been completed for this step?
+  bool pivotsDone;
+  // State of the L and U input blocks for the next update
+  struct InputState {
+    BlockReadyMsg *m;
+    double *data;
+    enum {
+      PENDING_SPACE, ALLOCATED, PENDING_REQUEST, REQUESTED, ARRIVED
+    } state;
+
+    InputState() : m(NULL), data(NULL), state(PENDING_SPACE) {}
+
+    void reset() {
+      delete m;
+      m = NULL;
+      data = NULL;
+      state = PENDING_SPACE;
+    }
+  } Lstate, Ustate;
+
+  BlockState(CkIndex2D index)
+    : ix(index.x), iy(index.y), updatesCompleted(0), pivotsDone(false) {}
+};
+
+typedef std::list<BlockState> StateList;
+
 class BlockScheduler : public CBase_BlockScheduler {
 public:
   BlockScheduler(CProxy_LUBlk luArr_)
@@ -22,31 +50,12 @@ public:
   void registerBlock(CkIndex2D index);
   void pivotsDone(CkIndex2D index);
   void dataReady(CkIndex2D index, BlockReadyMsg *m);
+  void updateDone(CkIndex2D index);
 
 private:
-  struct BlockState {
-    // Index of block
-    int ix, iy;
-    // Count of trailing updates completed
-    int updatesCompleted;
-    // Have pivots been completed for this step?
-    bool pivotsDone;
-    // State of the L and U input blocks for the next update
-    struct InputState {
-      BlockReadyMsg *m;
-      double *data;
-      enum {
-        PENDING_SPACE, ALLOCATED, PENDING_REQUEST, REQUESTED, ARRIVED
-      } state;
-
-      InputState() : m(NULL), data(NULL), state(PENDING_SPACE) {}
-    } Lstate, Ustate;
-
-    BlockState(CkIndex2D index)
-      : ix(index.x), iy(index.y), updatesCompleted(0), pivotsDone(false) {}
-  };
-
-  std::list<BlockState> localBlocks;
+  StateList localBlocks;
 
   CProxy_LUBlk luArr;
+
+  StateList::iterator findBlockState(CkIndex2D index);
 };
--- scheduler.C
+++ scheduler.C
@@ -1,20 +1,46 @@
 #include "lu.decl.h"
 #include "lu.h"
+#include <algorithm>
 
 void BlockScheduler::registerBlock(CkIndex2D index) {
+  localBlocks.push_back(BlockState(index));
+}
 
+struct findByIndex {
+  bool operator()(const BlockState &state) {
+    return state.ix == index.x && state.iy == index.y;
+  }
+
+  CkIndex2D index;
+  findByIndex(CkIndex2D _index) : index(_index) {}
+};
+
+StateList::iterator BlockScheduler::findBlockState(CkIndex2D index) {
+  StateList::iterator iter = find_if(localBlocks.begin(),
+                                     localBlocks.end(), findByIndex(index));
+  CkAssert(iter != localBlocks.end());
+  return iter;
 }
 
 void BlockScheduler::pivotsDone(CkIndex2D index) {
-
+  findBlockState(index)->pivotsDone = true;
 }
 
 void BlockScheduler::dataReady(CkIndex2D index, BlockReadyMsg *m) {
+  StateList::iterator iter = findBlockState(index);
+  BlockState::InputState &input = m->src.x == index.x ? iter->Lstate : iter->Ustate;
+  input.m = m;
+}
 
+void BlockScheduler::updateDone(CkIndex2D index) {
+  StateList::iterator iter = findBlockState(index);
+  iter->updatesCompleted++;
+  iter->pivotsDone = false;
+  iter->Lstate.reset();
+  iter->Ustate.reset();
 }
 
 inline bool operator==(const CkIndex2D &l, const CkIndex2D &r)
 { return l.x == r.x && l.y == r.y; }
 inline bool operator<(const CkIndex2D &l, const CkIndex2D &r)
 { return l.x < r.x || (l.x == r.x && l.y < r.y); }
-
