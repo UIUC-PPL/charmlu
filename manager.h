0a5e5ca1f6e42e1720003048695da667504c3569
Jonathan Lifflander <jliffl2@illinois.edu>
2011-03-04 23:12:58 -0600

    Prioritize some pivot messages because this helps create more work.

--- manager.h
+++ manager.h
@@ -81,13 +81,13 @@ struct PrioLU : public LUMgr
       prio = -1;
       break;
     case PIVOT_RIGHT_SEC:
-      prio = y * BLKSIZE;
+      prio = -1;
       break;
     case PIVOT_LEFT_SEC:
       prio = BLKSIZE * numBlks + 1;
       break;
     case PIVOT_CRITICAL:
-      prio = (y - 1) * BLKSIZE;
+      prio = -1;
       break;
     case PIVOT_NOT_CRITICAL:
       prio = numBlks * BLKSIZE;
