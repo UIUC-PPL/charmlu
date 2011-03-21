e83daf3a10b4a0a7f22e4637c25c8c537e8ae0b8
Jonathan Lifflander <jliffl2@illinois.edu>
2011-03-21 16:00:18 -0500

    Modified script to use gnuplot and include the TU model.

--- extractTimings.pl
+++ extractTimings.pl
@@ -6,7 +6,31 @@ use warnings;
 my %blockTimingStart;
 my %blockTimingStop;
 
-for (<>) {
+die "usage: <filename> <x-axis> <y-axis>" if ($#ARGV + 1 < 1);
+
+my $file = $ARGV[0];
+shift @ARGV;
+
+my $xaxis = 0;
+my $yaxis = 0;
+
+if ($#ARGV + 1 > 0) {
+    ($xaxis, $yaxis) = @ARGV;
+}
+
+open FILE, ">", "temp" or die $!;
+open RFILE, "<", "$file";
+
+my $chareArray;
+my $PEs;
+
+for (<RFILE>) {
+    if (/Chare Array size: (\d+) X/) {
+        $chareArray = $1;
+    }
+    if (/RESULT procs:\s+(\d+)/) {
+        $PEs = $1;
+    }
     if (/^Block (\d+) queueing local LU at internalStep \d+, start time = \d+\.\d+, time = (\d+\.\d+)$/) {
         $blockTimingStart{$1} = $2;
     }
@@ -27,7 +51,41 @@ for my $key (sort {$a <=> $b} (keys %blockTimingStart)) {
             $delay = $timeStart - $timeStopPrev;
         }
         if ($diff >= 0 and $delay >= 0) {
-            print "$key, $diff, $delay\n";
+            my $DGEMMsecs = 0.03;
+            my $TUmodel = (($chareArray-$key-1)*($chareArray-$key-1)/$PEs)*$DGEMMsecs;
+            print FILE "$key $diff $delay $TUmodel\n";
         }
     }
 }
+
+close FILE;
+close RFILE;
+
+# Now output for gnuplot
+my $plotCmds = <<END;
+set terminal pdf
+END
+
+if ($xaxis == 0) {
+  $plotCmds .= "set autoscale x\n";
+} else {
+  $plotCmds .= "set xrange [0:$xaxis]\n";
+}
+
+if ($yaxis == 0) {
+  $plotCmds .= "set autoscale y\n";
+} else {
+  $plotCmds .= "set yrange [0:$yaxis]\n";
+}
+
+$plotCmds .= <<END;
+set title '$file'
+set xlabel 'Execution Time (seconds)'
+set ylabel 'LU Step'
+set pointsize 0.5
+plot 'temp' using 1:2 title 'Active panel time' with points lt 1 pt 5, \\
+     'temp' using 1:3 title 'Time outside active panel' with points lt 2 pt 9, \\
+     'temp' using 1:4 title 'Trailing update time per step' with lines lw 2
+END
+
+print "$plotCmds";
