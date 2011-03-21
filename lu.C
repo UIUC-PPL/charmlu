39d949cc27a2b6acc200cc262067ddc99f169f78
Jonathan Lifflander <jliffl2@illinois.edu>
2011-03-20 23:15:01 -0500

    Added percent of peak for Ranger.

--- lu.C
+++ lu.C
@@ -450,6 +450,7 @@ public:
     } peaks[] = {{"order.cs.uiuc.edu", 7.4585},
 		 {"abe.ncsa.uiuc.edu", 9.332},
 		 {"Kraken", 10.4},
+                 {"Ranger", 9.2},
                  {"Jaguar XT5", 10.3987},
 		 {"BG/P", 3.4}};
 
