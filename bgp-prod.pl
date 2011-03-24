57bd68716fa048c1ff70dbb2dff87711ed0a60ca
Ramprasad Venkataraman <ramv@illinois.edu>
2011-03-24 20:31:09 +0000

    jobscripts: Teach the bgp scripts about peStride

--- bgp-prod.pl
+++ bgp-prod.pl
@@ -3,19 +3,20 @@
 use strict;
 use warnings;
 
-my $N =  256;
-my $mode = "vn";
-my $runnum = "262";
-my $BLK = 300;
-my $matrix = 96000;
-my $thresh = 310;
-my $agglom = 100;
-my $mapargs = "3 128 8";
+my $mode     = "vn";
+my $runnum   = "321";
+my $N        = 512;
+my $matrix   = 96000;
+my $BLK      = 150;
+my $agglom   = 150;
+my $mapargs  = "3 256 16";
 my $peRotate = 16;
+my $peStride = 2;
+my $thresh   = 310;
 
 
 print "Starting job $runnum on $N nodes, " . ($mode eq "vn" ? $N * 4 : $N) . " processors\n";
-print "inputs: $matrix $BLK $thresh $agglom $mapargs $peRotate\n";
+print "inputs: $matrix $BLK $thresh $agglom $mapargs $peRotate $peStride\n";
 
 my $dcmf = 16*1024*1024;
-`qsub --env "BG_MAXALIGNEXP=1000:DCMF_RECFIFO=$dcmf" -A PARTS --mode $mode -n $N -t 55 -o run-$runnum-$N-$BLK.out -e run-$runnum-$N-$BLK.error ./lu.prod $matrix $BLK $thresh $agglom $mapargs $peRotate +LBOff`;
+`qsub --env "BG_MAXALIGNEXP=1000:DCMF_RECFIFO=$dcmf" -A PARTS --mode $mode -n $N -t 25 -o run-$runnum-$N-$BLK.out -e run-$runnum-$N-$BLK.error ./redn.lu.prod $matrix $BLK $thresh $agglom $mapargs $peRotate $peStride +LBOff`;
