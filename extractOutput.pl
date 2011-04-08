e55c1f94b7c9b042324f8693be552a1d7a344db1
Jonathan Lifflander <jliffl2@illinois.edu>
2011-04-08 04:46:35 -0500

    Fix script to parse file name correctly.

--- extractOutput.pl
+++ extractOutput.pl
@@ -12,17 +12,29 @@ foreach my $file (@files) {
     my $runnum = -1;
     my $jobid = -1;
     my $time = -1;
-    if ($file =~ /[A-Za-z\-]*([0-9]+)\.o([0-9]+).*/) {
-        $runnum = $1;
-        $jobid = $2;
+    my @filepath = split '/', $file;
+    my $filename = $filepath[(length @filepath) - 2];
+    $filename =~ s/\./-/;
+    my @parts = split '-', $filename;
+
+    foreach (@parts) {
+        if (/^([0-9]+)/) {
+            $runnum = int($1);
+        }
+        if (/o([0-9]+)/) {
+            $jobid = int($1);
+        }
     }
+
+    if ($runnum == -1 || $jobid == -1) { next; }
+
     for (<FILE>) {
         if (/Time\(s\):\s+([0-9.]+)/) {
             $time = $1;
         }
         # Assumption that time always comes before peak: should hold because
         # main chare prints this in order
-        if (/Jaguar/) {
+        if (/Jaguar/ && $time != -1) {
             my @lst = split ' ', $_;
             my $percent = substr $lst[9], 0, (length $lst[9]) - 1;
             print "$runnum, $jobid, $time, $percent\n";
