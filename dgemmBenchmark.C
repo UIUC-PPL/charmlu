5a76031096f0ac9aeb96a13ffc5fae56d161fdef
Anshu Arya <arya3@illinois.edu>
2011-04-13 13:22:53 -0500

    dgemm benchmarks

---
+++ dgemmBenchmark.C
@@ -0,0 +1,81 @@
+#include "benchmark.decl.h"
+#include <vector>
+#include <algorithm>
+#include "acml.h"
+
+CProxy_Main mainProxy;
+int blockSize;
+int numIter;
+
+struct Main : public CBase_Main {
+  CProxy_dgemmTest dgemms;
+  std::vector<double> peTimes;
+
+  Main(CkArgMsg *m) : peTimes(CkNumPes()) {
+    if (m->argc < 3) {
+      CkPrintf("usage: blockSize numIter\n");
+      CkExit();
+    }
+
+    blockSize = atoi(m->argv[1]);
+    numIter = atoi(m->argv[2]);
+
+    CkPrintf("blockSize = %d, numIter = %d\n",blockSize, numIter);
+
+    mainProxy = thisProxy;
+    dgemms = CProxy_dgemmTest::ckNew();
+    dgemms.testAndSync();
+  }
+
+  void finishedTest(int pe, double time) {
+    peTimes[pe] = time;
+    if (++count == CkNumPes()) {
+
+        for (int i = 0; i < CkNumPes(); ++i) {
+            double dgemmDuration    = peTimes[i]/numIter;
+            double dgemmFlopCount   = (double)blockSize * (double)blockSize * (double)blockSize * 2.0;
+            double dgemmGFlopCount  = dgemmFlopCount / 1000000000.0;
+            double dgemmGFlopPerSec = dgemmGFlopCount / dgemmDuration;
+
+            CkPrintf("The dgemm %d x %d takes %g ms and achieves %g GFlop/sec, %g percent of peak on Jaguar\n", blockSize,
+            blockSize, dgemmDuration*1000, dgemmGFlopPerSec, dgemmGFlopPerSec/10.3987);
+        }
+
+        CkExit();
+    }
+  }
+};
+
+struct dgemmTest : public CBase_dgemmTest {
+
+  double *m1, *m2, *m3;
+
+  dgemmTest() {
+    m1 = new double[blocksize*blocksize];
+    m2 = new double[blocksize*blocksize];
+    m3 = new double[blocksize*blocksize];
+  }
+
+  void testAndSync() {
+
+    double startTestTime = CmiWallTimer();
+
+    for(int i=0; i < numIter; i++) {
+        dgemm(BLAS_NOTRANSPOSE, BLAS_NOTRANSPOSE,
+            blocksize, blocksize, blocksize,
+            -1.0, m1,
+            blocksize, m2, blocksize,
+            1.0, m3, blocksize);
+    }
+
+    double totalTestTime = CmiWallTimer() - startTestTime;
+
+    delete[] m1;
+    delete[] m2;
+    delete[] m3;
+
+    mainProxy.finishedTest(CkMyPe(), totalTestTime);
+  }
+};
+
+#include "benchmark.def.h"
