706444dc8ccf8692f780742f4f7ff7d14d7b6cc1
Ramprasad Venkataraman <ramv@illinois.edu>
2012-10-15 11:40:24 -0500

    build: nuke dead machines from config.mk

--- config.mk
+++ config.mk
@@ -5,30 +5,12 @@ BLAS_INC  = -DUSE_CBLAS_H=1
 BLAS_LD   =
 BLAS_LIBS = -lcblas
 
+
 # To compile on hope (OSX):
 #SEND_LIM ?= 2
 #BLAS_INC  = -DUSE_ACCELERATE_BLAS=1
 #BLAS_LD   =
 #BLAS_LIBS = -framework Accelerate
-
-# To compile against a custom atlas install on linux:
-#SEND_LIM ?= 2
-#BLAS_INC  = -I/scratch/idooley2/atlas-install/include  -DUSE_CBLAS_H=1
-#BLAS_LD   = -L/scratch/idooley2/atlas-install/lib
-#BLAS_LIBS = -llapack -lf77blas -lcblas -latlas
-
-
-# To compile with mkl on abe
-#SEND_LIM ?= 2
-#BLAS_INC  = -DUSE_MKL_CBLAS_H=1 -I${MKL_HOME}/include/
-#BLAS_LD   = -L${MKL_HOME}/lib/em64t
-#BLAS_LIBS = -lmkl_intel_lp64 -lmkl_sequential -lmkl_core
-
-# To compile with ATLAS on abe
-#SEND_LIM ?= 2
-#BLAS_INC  = -I/u/ac/idooley2/LU/atlas-install/include -DUSE_CBLAS_H=1
-#BLAS_LD   = -L/u/ac/idooley2/LU/atlas-install/lib
-#BLAS_LIBS = -llapack -lf77blas -lcblas -latlas
 
 
 # To compile on Cray XT5 with "module load atlas/3.8.3"
@@ -49,6 +31,7 @@ BLAS_LIBS = -lcblas
 #BLAS_LD   = -L$(ACML_DIR)/gnu64/lib/
 #BLAS_LIBS = -lacml
 
+
 # To compile on BG/P with ESSL:
 #SEND_LIM ?= 50
 #BGP_ESSL  = /soft/apps/ESSL-4.4.1-0
@@ -60,6 +43,7 @@ BLAS_LIBS = -lcblas
 #BLAS_INC  = -DUSE_ESSL=1 -I$(BGP_ESSL)/include -DUSE_MEMALIGN=1
 #BLAS_LD   = $(BGP_LIBS)
 #BLAS_LIBS = -lesslbg -lmass -lxlfmath -lxlf90_r -lxlsmp -lxlomp_ser -lpthread
+
 
 # To compile on BG/Q with ESSL:
 #SEND_LIM ?= 500
